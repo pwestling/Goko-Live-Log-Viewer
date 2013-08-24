5c952570551ff14fc0faa711c2e76b363cd5c3ab
U-pwestling-PC\pwestling <pwestling@pwestling-PC.(none)>
2013-08-23 21:52:55 -0700

    version increase

--- Goko_Live_Log_Viewer.user.js
+++ Goko_Live_Log_Viewer.user.js
@@ -7,7 +7,7 @@
 // @require     http://dom.retrobox.eu/js/1.0.0/set_parser.js
 // @run-at      document-end
 // @grant       none
-// @version     15
+// @version     16
 // ==/UserScript==
 var foo = function () {
 if (Dom.LogManager.prototype.old_addLog) {
