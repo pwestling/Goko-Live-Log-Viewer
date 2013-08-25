ca5b1aa4082617ce6635fc466394f0365afeb55b
Porter Westling <pwestling@liveramp.com>
2013-08-25 13:11:15 -0700

    increment version

--- Goko_Live_Log_Viewer.user.js
+++ Goko_Live_Log_Viewer.user.js
@@ -7,7 +7,7 @@
 // @require     http://dom.retrobox.eu/js/1.0.0/set_parser.js
 // @run-at      document-end
 // @grant       none
-// @version     31
+// @version     32
 // ==/UserScript==
 var foo = function () {
         if (Dom.LogManager.prototype.old_addLog) {
